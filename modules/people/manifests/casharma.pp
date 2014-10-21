22754b4fdc1dbdda0b470083aa7cbe338aecfa2c
Carthik Sharma <carthik@luttapi.home>
2014-10-20 17:51:52 -0700

    use zsh

--- modules/people/manifests/casharma.pp
+++ modules/people/manifests/casharma.pp
@@ -1,5 +1,11 @@
 class people::casharma {
   notify { "doing @casharma's bidding": }
+	
+  # set my shell to zsh
+  osx_chsh { $::luser:
+    shell   => '/opt/boxen/homebrew/bin/zsh',
+    require => Package['zsh'],
+  }
 
 
 }
