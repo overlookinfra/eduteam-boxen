afae34a8956321e66e918cdaee24bbac488b69a2
Carthik Sharma <carthik@luttapi.home>
2014-10-20 18:45:18 -0700

    added zsh to casharma's manifest

--- modules/people/manifests/casharma.pp
+++ modules/people/manifests/casharma.pp
@@ -2,10 +2,6 @@ class people::casharma {
   notify { "doing @casharma's bidding": }
 	
   # set my shell to zsh
-  osx_chsh { $::luser:
-    shell   => '/opt/boxen/homebrew/bin/zsh',
-    require => Package['zsh'],
-  }
-
+  include zsh
 
 }
