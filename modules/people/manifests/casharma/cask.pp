dbad17da6fbffca8ba1b63c5bc7b00df9f5584b2
Carthik Sharma <carthik@puppetlabs.com>
2014-10-22 22:11:54 -0700

    couple more apps

--- modules/people/manifests/casharma/cask.pp
+++ modules/people/manifests/casharma/cask.pp
@@ -1,14 +1,20 @@
 class people::casharma::cask {
 
   $casks = [
+    'caffeine',
+    'cord',
     'cyberduck',
     'day-o',
     'handbrakebatch',
     'hipchat',
+    'launchbar',
     'macdown',
+    'mplayer-osx-extended',
+    'private-internet-access',
     'screenflow',
     'skype',
     'spectacle',
+    'twitterific,
     'utorrent'
   ]
 
