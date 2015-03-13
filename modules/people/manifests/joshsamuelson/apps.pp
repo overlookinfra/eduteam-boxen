6aeb33d30caa6eb7c1957cd313bce3a181418ba8
Josh Samuelson <js@puppetlabs.com>
2015-03-13 16:17:17 -0700

    whitespace

--- modules/people/manifests/joshsamuelson/apps.pp
+++ modules/people/manifests/joshsamuelson/apps.pp
@@ -1,6 +1,4 @@
 class people::joshsamuelson::apps {
   $boxen_apps = hiera('people::joshsamuelson::apps::boxen_apps')
-
   include $boxen_apps
-
 }
--- modules/people/manifests/joshsamuelson/preferences.pp
+++ modules/people/manifests/joshsamuelson/preferences.pp
@@ -1,5 +1,4 @@
 class people::joshsamuelson::preferences {
-
   $prefs = hiera('people::joshsamuelson::preferences::personal_prefs')
   include $prefs
 }
