00b7342569d65dea59925409326d10bfe3837530
Josh Samuelson <js@puppetlabs.com>
2015-03-13 16:15:26 -0700

    moved brew apps to hiera

--- modules/people/manifests/joshsamuelson/brew.pp
+++ modules/people/manifests/joshsamuelson/brew.pp
@@ -1,17 +1,6 @@
 class people::joshsamuelson::brew {
-  $formulae = [
-    'tree',
-    'wget',
-    'gnupg',
-    'graphviz',
-    'fontconfig',
-    'fontforge',
-    'imagemagick',
-    'jasper',
-    'tmux',
-    'watch',
-    'xorriso'
-  ]
+  $formulae = hiera('people::joshsamuelson::brew::formulae')
+
   package { $formulae :
    ensure => present,
   }
