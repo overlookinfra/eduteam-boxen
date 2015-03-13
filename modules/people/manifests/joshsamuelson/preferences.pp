2fdae3443369650d03741d4c0fc8ba71710a2045
Josh Samuelson <js@puppetlabs.com>
2015-03-13 16:12:24 -0700

    moved prefs to hiers

--- modules/people/manifests/joshsamuelson/preferences.pp
+++ modules/people/manifests/joshsamuelson/preferences.pp
@@ -1,26 +1,5 @@
 class people::joshsamuelson::preferences {
 
-  osx::recovery_message { 'If found, please contact Josh Samuelson - josh@joshsamuelson.com 206.201.1742': }
-  include osx::dock::dim_hidden_apps
-  include osx::dock::autohide
-  include osx::dock::hot_corners
-  include osx::dock::icon_size
-  include osx::dock::position
-  include osx::global::disable_autocorrect
-  include osx::global::natural_mouse_scrolling
-  include osx::global::tap_to_click
-  include osx::disable_app_quarantine
-  include osx::software_update
-
-  # FIXME: the following are done manually for now
-  # Use the Dark UI
-  # magnification for the dock
-  # minimize windows into app icon
-  # disable date/time display in menu bar - but after day-o honors dark
-  # battery in menu bar - show %
-  # always show scrollbar
-  # ask to keep changes when quitting an app
-  # Prefs -> KB -> Shortcuts -> Services -> New Terminal at Folder.
-
-  
+  $prefs = hiera('people::joshsamuelson::preferences::personal_prefs')
+  include $prefs
 }
